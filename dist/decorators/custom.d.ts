c3911ff43a2c81908a05635209a52261b175a430
Jacob Oakes <jacoboakes92@gmail.com>
2018-02-12 09:35:27 -0500

    Release 2.2.1

---
+++ dist/decorators/custom.d.ts
@@ -0,0 +1,2 @@
+export declare function CustomParameter(getParam: (req: any) => any): (target: object, key: string | symbol, index: number) => void;
+export declare function CustomParameters(): (target: object, key: string | symbol, descriptor: any) => any;
